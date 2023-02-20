531baa9f6f4325a189b48797ffa78bfee6f34ca2
Harun Nurahman <harun.nurrahman@hotmail.com>
2023-02-20 15:44:45 +0700

    Creating Model

---
+++ lib/models/weather_data.dart
@@ -0,0 +1,14 @@
+import 'package:weather_app/models/weather_data_current.dart';
+import 'package:weather_app/models/weather_data_daily.dart';
+import 'package:weather_app/models/weather_data_hourly.dart';
+
+class WeatherData {
+  final WeatherDataCurrent? current;
+  final WeatherDataHourly? hourly;
+  final WeatherDataDaily? daily;
+
+  WeatherData([this.current, this.hourly, this.daily]);
+  WeatherDataCurrent getCurrentWeather() => current!;
+  WeatherDataHourly getHourlyWeather() => hourly!;
+  WeatherDataDaily getDailyWeather() => daily!;
+}
---
+++ lib/services/api_services.dart
@@ -0,0 +1,8 @@
+// class apiServices() {
+//   final Dio _dio = Dio();
+
+//   final String baseUrl = 'https://api.openweathermap.org/data/3.0';
+//   final String apiKey = '2c8848a279b83b708bd1998475800a02';
+
+
+// }
